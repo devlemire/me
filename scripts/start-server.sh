b2bb7469c54a77ec1eb7882c54ac6e2df05a5cd0
= <webdev.jameslemire@gmail.com>
2018-07-10 16:25:30 -0600

    JL master - Add a deploy script.

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
-SCRIPT_DIR="$(pwd)/$(dirname $0)"
-
-cd $SCRIPT_DIR/../
-
-yarn
-
-cd $SCRIPT_DIR/../public
-
-yarn
-
-yarn build
-
 forever stop me
-forever start -l me.log -a --uid me $SCRIPT_DIR/../index.js
+forever start -l me.log -a --uid me $1/../index.js
