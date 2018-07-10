54eee1e148d6dfaaf4f9f9924ec0a76e8a0480d5
= <webdev.jameslemire@gmail.com>
2018-07-10 14:27:35 -0600

    JL master - Update start-server script.

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,8 +4,7 @@ cd "$(pwd)/$(dirname $0)"/../public
 
 yarn build
 
+cd "$(pwd)/$(dirname $0)"
+
 forever stop me
-
-echo "$(pwd)/$(dirname $0)"
-
 forever start -l me.log -a --uid me "$(pwd)/$(dirname $0)"/../index.js
