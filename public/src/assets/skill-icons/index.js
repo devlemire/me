0dc7f0d9f69cf86eaec1e90428190c0e8ccf1c7c
= <webdev.jameslemire@gmail.com>
2018-07-10 20:33:54 -0600

    JL master - Add skills to Home page.

---
+++ public/src/assets/skill-icons/index.js
@@ -0,0 +1,27 @@
+import html5 from './html5.png'
+import react from './react.png'
+import vue from './vue.png'
+import css3 from './css3.png'
+import psql from './psql.png'
+import node from './nodejs.png'
+import javascript from './javascript.png'
+import github from './github.svg'
+import express from './express.png'
+import docker from './docker.png'
+import travis from './travis.png'
+import digitalocean from './digitalocean.png'
+
+export {
+  html5,
+  react,
+  vue,
+  css3,
+  psql,
+  node,
+  javascript,
+  github,
+  express,
+  docker,
+  travis,
+  digitalocean
+}
--- public/src/components/Landing/Landing.js
+++ public/src/components/Landing/Landing.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import meImg from '../../assets/me.jpg'
+
+import skills from '../../utils/skills'
 
 export default function Landing() {
+  let skillsJSX = skills.map((skill, i) => (
+    <div key={`skill-${i}`} className="Landing-skill">
+      <img src={skill.img} alt="skill" width="50px" />
+      <h2>{skill.label}</h2>
+    </div>
+  ))
+
   return (
-    <div>
-      <span>I am Landing</span>
+    <div id="Landing-content">
+      <div id="Landing-about">
+        <div id="about-top">
+          <img
+            id="about-profile-picture"
+            src={meImg}
+            alt="profile"
+            height="75px"
+          />
+
+          <h1>About Me</h1>
+        </div>
+
+        <div id="about-bottom">
+          <p>
+            Hello my name is James Lemire, I'm 21 years old and from the Daytona
+            Beach area in Florida. Tired of bussing tables back home I flew out
+            to Utah to pursue a career in Web Development and a better way of
+            life. I've been out here in Utah for about two years now and have
+            fallen in love with full stack web development. I'm always
+            interested in hearing new opportunities that involve full stack
+            development with React or Vue and NodeJS. These technologies are my
+            specialty! I have a strong passion for programming and have been
+            doing it off and on since I was 12 years old.
+          </p>
+        </div>
+      </div>
+
+      <div id="Landing-skills" className="black-container">
+        <h1>My Skills</h1>
+        <div id="Landing-skills-list">{skillsJSX}</div>
+      </div>
     </div>
   )
 }
