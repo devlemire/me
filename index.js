cb5f65311d7f028cdd08a37497e3c9d1f70e31bb
= <webdev.jameslemire@gmail.com>
2018-07-16 13:09:57 -0600

    JL master - Remove dummy stuff

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
 require('dotenv').config({ path: __dirname + '/.env' })
 const express = require('express')
 const path = require('path')
-const bodyParser = require('body-parser')
-const util = require('util')
-const xml = require('xml')
-require('body-parser-xml')(bodyParser)
 
 const { NODE_ENV, SERVER_PORT } = process.env
 
-// enrollment, application, contact
-// if they change email
-
 const app = express()
-
-app.use(bodyParser.xml())
 
 app.get('/api/test', (req, res) => {
   res.send('yaba daba dooo!')
-})
-
-app.post('/api/test', (req, res) => {
-  console.log({ body: req.body, params: req.params, query: req.query })
-  console.log(
-    util.inspect(req.body['soapenv:Envelope']['soapenv:Body'], false, null)
-  )
-
-  var js = {
-    element: [
-      { _attr: { name: 'notificationsResponse' } },
-      {
-        complexType: [
-          {
-            sequence: [
-              { element: [{ _attr: { name: 'Ack', type: 'xsd:boolean' } }] }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-
-  res.set('Content-Type', 'text/xml')
-  res.send(xml(js))
 })
 
 // Serve front end files
